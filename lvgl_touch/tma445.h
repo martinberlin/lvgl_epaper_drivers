5ce190a05035891cda3294ef80962b581e4ae3c6
martinberlin <martin@luckycloud.de>
2024-04-11 07:10:22 +0200

    First raw version of TMA445 Cypress touch

---
+++ lvgl_touch/tma445.h
@@ -0,0 +1,53 @@
+#ifndef __TMA445_H
+/*
+* Copyright © 2024 Fasani Corp.
+
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+* software and associated documentation files (the “Software”), to deal in the Software 
+* without restriction, including without limitation the rights to use, copy, modify, merge, 
+* publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+* to whom the Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all copies or 
+* substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+* FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+* SOFTWARE.
+*/
+
+#define __TMA445_H
+
+#include <stdint.h>
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+#include "lvgl.h"
+#else
+#include "lvgl/lvgl.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+  * @brief  Initialize for GT911 communication via I2C
+  * @param  dev_addr: Device address on communication Bus (I2C slave address of GT911).
+  * @retval None
+  */
+void tma445_init(uint8_t dev_addr);
+
+/**
+  * @brief  Get the touch screen X and Y positions values. Ignores multi touch
+  * @param  drv:
+  * @param  data: Store data here
+  * @retval Always false
+  */
+bool tma445_read(lv_indev_drv_t *drv, lv_indev_data_t *data);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* __GT911_H */
